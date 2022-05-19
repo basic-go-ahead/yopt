c3e00fbd0aa50620a22b729f0aa05902431dfdfe
basic-go-ahead <fulu.happy@gmail.com>
2022-05-19 20:12:40 +0300

    Put md into internals

--- yopt/internals.py
+++ yopt/internals.py
@@ -13,8 +13,7 @@ def _sgd_fit(
     weights: ArrayLike,
     done_steps: int
 ):
-    n = X.shape[0]
-    T = n * n_passes
+    T = X.shape[0] * n_passes
 
     y -= .5
     y *= 2
@@ -25,7 +24,7 @@ def _sgd_fit(
         indices = np.random.permutation(n)
         for i in indices:
             done_steps += 1
-            curr_ŋ = start_ŋ / (done_steps + 1.)
+            curr_ŋ = start_ŋ / done_steps
             features, target = X[i], y[i]
             d = np.dot(weights, features) * target
             weights *= 1. - curr_ŋ
@@ -37,3 +36,45 @@ def _sgd_fit(
 
     weights = acc / T
     return done_steps
+
+
+
+@jit(nopython=True)
+def _md_online_fit(
+    X: ArrayLike,
+    y: ArrayLike,
+    n_passes: int,
+    reg_λ: float,
+    start_ŋ: float,
+    weights: ArrayLike,
+    done_steps: int,
+    inv_matrix: ArrayLike,
+    md_strategy: int
+):
+    y -= .5
+    y *= 2
+
+    for k in range(n_passes):
+        for features, target in zip(X, y):
+            done_steps += 1
+
+            if md_strategy == 0:
+                curr_ŋ = start_ŋ
+            else:
+                curr_ŋ = start_ŋ / done_steps
+
+            d = np.dot(weights, features) * target
+
+            if d <= 1.:
+                if inv_matrix is None:
+                    weights += target * curr_ŋ * features
+                else:
+                    weights += inv_matrix @ (target * curr_ŋ * features)
+
+                norm = np.linalg.norm(weights)
+                if norm > 1.:
+                    weights /= norm
+
+    return done_steps
+
+
--- yopt/svm.py
+++ yopt/svm.py
@@ -2,11 +2,16 @@ import numpy as np
 
 from sklearn.multiclass import OneVsOneClassifier
 
+import enum
 from typing import Any, Dict, Union
 from numpy.typing import ArrayLike
 
-from .internals import _sgd_fit
+from .internals import _sgd_fit, _md_online_fit
 
+
+class MirrorDescentStepStrategy(enum.IntEnum):
+    CONST = 0
+    DECREASING = 1
 
 
 class SVMBinaryClassifier:
@@ -17,7 +22,8 @@ class SVMBinaryClassifier:
     def __init__(self, reg_λ: float=1., start_ŋ: float=1e-1, n_passes: int=10,
         method: str='sgd',
         mode: str='offline',
-        md_inv_matrix: Union[ArrayLike, None]=None
+        md_inv_matrix: Union[ArrayLike, None]=None,
+        md_strategy: MirrorDescentStepStrategy=MirrorDescentStepStrategy.CONST
     ):
         assert reg_λ > 0
         assert start_ŋ > 0
@@ -30,6 +36,7 @@ class SVMBinaryClassifier:
         self.method = method
         self.mode = mode
         self.md_inv_matrix = md_inv_matrix
+        self.md_strategy = md_strategy
         
 
     def get_params(self, deep: bool=True) -> Dict[str, Any]:
@@ -39,7 +46,8 @@ class SVMBinaryClassifier:
             n_passes=self.n_passes,
             method=self.method,
             mode=self.mode,
-            md_inv_matrix=self.md_inv_matrix
+            md_inv_matrix=self.md_inv_matrix,
+            md_strategy=self.md_strategy
         )
 
 
@@ -56,42 +64,51 @@ class SVMBinaryClassifier:
         y = np.asarray(y, dtype=np.float64).copy()
 
         if self._weights is None or self.mode == 'offline':
-            self._weights = np.zeros(X.shape[1], dtype=np.float64)
+            self._weights = np.zeros(X.shape[1], dtype=np.float64, order='C')
             self._done_steps = 0
 
         if self.method == 'sgd':
             self._done_steps = _sgd_fit(X, y, self.n_passes, self.reg_λ, self.start_ŋ, self._weights, self._done_steps)
         elif self.method == 'md':
             n = X.shape[0]
-            T = n * self.n_passes
 
-            weights = self._weights
+            self._done_steps = _md_online_fit(X, y,
+                self.n_passes,
+                self.reg_λ,
+                self.start_ŋ,
+                self._weights,
+                self._done_steps,
+                self.md_inv_matrix,
+                self.md_strategy
+            )
 
-            y -= .5
-            y *= 2
+            # weights = self._weights
+
+            # y -= .5
+            # y *= 2
 
             # acc = np.zeros_like(weights)
 
-            done_steps = self._done_steps
+            # done_steps = self._done_steps
 
-            for k in range(self.n_passes):
-                for features, target in zip(X, y):
-                    done_steps += 1
-                    curr_ŋ = self.start_ŋ / (done_steps + 1.)
-                    # curr_ŋ = self.start_ŋ
-                    # features, target = X[i], y[i]
-                    d = np.dot(weights, features) * target
+            # for k in range(self.n_passes):
+            #     for features, target in zip(X, y):
+            #         done_steps += 1
+            #         curr_ŋ = self.start_ŋ / (done_steps + 1.)
+            #         # curr_ŋ = self.start_ŋ
+            #         # features, target = X[i], y[i]
+            #         d = np.dot(weights, features) * target
 
-                    if d <= 1.:
-                        if self.md_inv_matrix is None:
-                            weights += target * self.start_ŋ * features
-                        else:
-                            weights += self.md_inv_matrix @ (target * curr_ŋ * features)
+            #         if d <= 1.:
+            #             if self.md_inv_matrix is None:
+            #                 weights += target * self.start_ŋ * features
+            #             else:
+            #                 weights += self.md_inv_matrix @ (target * curr_ŋ * features)
 
-                        norm = np.linalg.norm(weights)
+            #             norm = np.linalg.norm(weights)
 
-                        if norm > 1.:
-                            weights /= norm
+            #             if norm > 1.:
+            #                 weights /= norm
 
                     # acc += weights
 
@@ -108,7 +125,7 @@ class SVMBinaryClassifier:
             #         # acc += weights
 
             # # weights = acc / T
-            self._done_steps = done_steps
+            # self._done_steps = done_steps
 
         return self
 
@@ -130,9 +147,10 @@ class SVMClassifier:
     def __init__(self, reg_λ: float=1., start_ŋ: float=1e-1, n_passes: int=10,
         method: str='sgd',
         mode: str='offline',
-        md_inv_matrix: Union[ArrayLike, None]=None
+        md_inv_matrix: Union[ArrayLike, None]=None,
+        md_strategy: MirrorDescentStepStrategy=MirrorDescentStepStrategy.CONST
     ):
-        self.svm = SVMBinaryClassifier(reg_λ, start_ŋ, n_passes, method, mode, md_inv_matrix)
+        self.svm = SVMBinaryClassifier(reg_λ, start_ŋ, n_passes, method, mode, md_inv_matrix, md_strategy)
         self.inner_clf = OneVsOneClassifier(self.svm)
 
 
